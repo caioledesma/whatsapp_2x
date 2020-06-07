7c2a23062d97480e4e01a3caf5b081079fed0a03
Caio Ledesma <42016457+caioledesma@users.noreply.github.com>
2020-06-07 20:48:19 -0300

    aplicação finalizada

---
+++ content.js
@@ -0,0 +1,35 @@
+//console.log("minha extensão")
+
+const interval = setInterval(() => {
+    const header = document.querySelector("._1QUKR");
+
+    if(header){
+        console.log(header)
+        clearInterval(interval) 
+    
+        const button = document.createElement("button")
+        button.innerHTML = "2X"
+        button.classList.add("button")
+
+        button.addEventListener("click", () => {
+            const audios = document.querySelectorAll("audio");
+            audios.forEach((audio) => {
+                audio.playbackRate = 2;
+            })
+
+        })
+
+        
+
+
+        header.appendChild(button)
+    
+
+        
+
+    }
+}, 1000)
+
+
+
+
